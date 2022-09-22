61c9846b6cdccf1d47b4e57db6f3c2f91193d371
wahyupratama27 <wahyupratamaputra4@gmail.com>
2022-09-22 10:53:44 +0700

    new

---
+++ app/src/main/java/com/example/projectbelajarintent/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.projectbelajarintent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void panggil(View view) {
+        String nomor = "09667347" ;
+        Intent panggil = new Intent(Intent. ACTION_DIAL);
+        panggil.setData(Uri. fromParts("tel",nomor,null));
+        startActivity(panggil);
+    }
+    public void tentang(View view) {
+        Intent tentang= new Intent(MainActivity.this, Tentang.class);
+        startActivity(tentang);
+    }
+    public void buka(View view) {
+        String url = "https://badoystudio.com" ;
+        Intent bukabrowser = new Intent(Intent. ACTION_VIEW);
+        bukabrowser.setData(Uri. parse(url));
+        startActivity(bukabrowser);
+    }
+}
